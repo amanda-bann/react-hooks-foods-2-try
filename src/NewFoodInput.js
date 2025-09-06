2ccc69884dd4d234ed1940e7a83198ecc205d1d4
amanda-bann <amanda.banner@gmail.com>
2025-09-05 18:11:12 -0600

    Create the food state & configure .gitignore

--- src/NewFoodInput.js
+++ src/NewFoodInput.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
-const NewFoodInput = () => {
+const NewFoodInput = (props) => {
+  const [food, setFood] = useState("");
+  
+  const updateFood = (e) => {
+    setFood(e.target.value);
+  };
+  
   return (
     <form className="food-form">
-      <input type="text" aria-label="Add a food to the list" />
+      <input type="text" aria-label="Add a food to the list" value={food} onChange={updateFood}/>
       <button>Add to the list!</button>
     </form>
   );
