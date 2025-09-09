f3ff490b42c215d4fb991a12bc159b3f6441db7a
amanda-bann <amanda.banner@gmail.com>
2025-09-09 11:19:09 -0600

    Allow the user to add a new food item

--- src/NewFoodInput.js
+++ src/NewFoodInput.js
@@ -6,9 +6,15 @@ const NewFoodInput = (props) => {
   const updateFood = (e) => {
     setFood(e.target.value);
   };
-  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addFoodItem(food);
+    setFood("");
+  };
+
   return (
-    <form className="food-form">
+    <form className="food-form" onSubmit={handleSubmit}>
       <input type="text" aria-label="Add a food to the list" value={food} onChange={updateFood}/>
       <button>Add to the list!</button>
     </form>
