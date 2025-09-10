b63f6a599a7da9afb2b0af2b9e23af7cf69b438b
amanda-bann <amanda.banner@gmail.com>
2025-09-09 21:52:26 -0600

    Add Event Handle to Delete Food

--- src/Food.js
+++ src/Food.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Food = (props) => {
-    return <li>{props.food}</li>;
+
+    // const onDelete = () => {
+    //     props.deleteFood(props.food)
+    // }
+
+    return (
+        <li>
+            {props.food}
+            <span className="delete" onClick={() => props.deleteFood(props.food)}>
+                {/* Alternate method: using a callback function to delete food. */}
+                X
+                </span>
+        </li>
+    );
 };
 
 export default Food;
